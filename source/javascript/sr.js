afb289a692032387b9c25ff9352c0b1de9e626d6
Rafael <rafael.machado.ampudia01@gmail.com>
2024-08-14 17:43:35 -0300

    new

---
+++ source/javascript/sr.js
@@ -0,0 +1,6 @@
+const sr=ScrollReveal({origin:"left",opacity:"0.7",distance:"100px",duration:1500,reset:!1});sr.reveal(`.home__data, .home__img,
+    .about__data, .about__img,
+    .services__content, .products__content,
+    .representative__data, .representative__img,
+    .contact__data, .contact__button,
+    .footer__content`,{interval:20});
