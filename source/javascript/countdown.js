da8545322aa7e424d1c93968c83e31e0955f6892
Rafael <rafael.machado.ampudia01@gmail.com>
2024-08-17 12:43:45 -0300

    Timer and about

--- source/javascript/countdown.js
+++ source/javascript/countdown.js
@@ -2,11 +2,8 @@
 
 const formatarDigito = (digito) => `0${digito}`.slice(-2);
 
-const atualizar = (tempo) => {
-  const segundos = document.getElementById("segundos");
-  const minutos = document.getElementById("minutos");
-  const horas = document.getElementById("horas");
-  const dias = document.getElementById("dias");
+const atualizar = (tempo, elementos) => {
+  const { segundos, minutos, horas, dias } = elementos;
 
   const qtdSegundos = tempo % 60;
   const qtdMinutos = Math.floor((tempo % (60 * 60)) / 60);
@@ -19,23 +16,41 @@ const atualizar = (tempo) => {
   dias.textContent = formatarDigito(qtdDias);
 };
 
-const contagemRegressiva = (tempo) => {
+const contagemRegressiva = (tempo, elementos) => {
   const pararContagem = () => clearInterval(id);
 
   const contar = () => {
-    if (tempo == 0) {
+    if (tempo <= 0) {
       pararContagem();
     }
-    atualizar(tempo);
+    atualizar(tempo, elementos);
     tempo--;
   };
   const id = setInterval(contar, 1000);
 };
 
-const tempoRestante = () => {
-  const dataEvento = new Date("2024-09-18 07:00:00");
+const tempoRestante = (dataEvento) => {
   const hoje = Date.now();
   return Math.floor((dataEvento - hoje) / 1000);
 };
 
-contagemRegressiva(tempoRestante());
+// Exemplo de como iniciar dois contadores
+const elementos1 = {
+  segundos: document.getElementById("segundos"),
+  minutos: document.getElementById("minutos"),
+  horas: document.getElementById("horas"),
+  dias: document.getElementById("dias"),
+};
+
+const elementos2 = {
+  segundos: document.getElementById("segundos-shirt"),
+  minutos: document.getElementById("minutos-shirt"),
+  horas: document.getElementById("horas-shirt"),
+  dias: document.getElementById("dias-shirt"),
+};
+
+const dataEvento1 = new Date("2024-09-18 07:00:00");
+const dataEvento2 = new Date("2024-09-14 00:00:00");
+
+contagemRegressiva(tempoRestante(dataEvento1), elementos1);
+contagemRegressiva(tempoRestante(dataEvento2), elementos2);
